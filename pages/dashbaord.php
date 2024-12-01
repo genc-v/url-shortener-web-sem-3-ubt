b1c3d588e6fbe29e0b87ce8101210d23117b27d3
Genc Vllahiu <genc.v@gjirafa.com>
2024-12-01 18:46:46 +0100

    wip

---
+++ pages/dashbaord.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>View Links</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="test.css">
+</head>
+
+<body>
+  <!-- Toast container -->
+  <div id="toast-container"></div>
+
+  <div class="urlList-container">
+    <h2 class="url-list__title">Your Previous URLs</h2>
+    <div class="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th id="originalUrl-header" onclick="sortTable('originalUrl')">Original URL</th>
+            <th>Short URL</th>
+            <th id="dateCreated-header" onclick="sortTable('dateCreated')">Date Created</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody id="url-table-body"></tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="modal-overlay" id="modal-overlay"></div>
+  <div class="modal" id="qr-modal">
+    <h3>QR Code</h3>
+    <div class="qr-code" id="qr-code"></div>
+    <div class="cta__buttons">
+      <button onclick="downloadQRCode()">Download QR Code</button>
+      <button class="close-button" onclick="closeModal()">Close</button>
+    </div>
+  </div>
+
+  <div class="modal-overlay" id="delete-modal-overlay"></div>
+  <div class="modal" id="delete-modal">
+    <h3>Are you sure you want to delete this URL?</h3>
+    <div class="cta__buttons">
+      <button id="confirm-delete" onclick="confirmDelete()">Yes</button>
+      <button onclick="closeDeleteModal()">No</button>
+    </div>
+  </div>
+
+  <div class="modal-overlay" id="edit-modal-overlay"></div>
+  <div class="modal" id="edit-modal">
+    <h3>Edit Description</h3>
+    <input type="text" id="edit-description-input" />
+    <div class="cta__buttons">
+
+      <button onclick="updateDescription()">Save</button>
+      <button onclick="closeEditModal()">Cancel</button>
+    </div>
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+  <script>
+    const token = localStorage.getItem('authToken');
+
+    const timeAgo = (date) => {
+      const now = new Date();
+      const past = new Date(date);
+      const seconds = Math.floor((now - past) / 1000);
+      const intervals = [
+        { label: 'year', seconds: 31536000 },
+        { label: 'month', seconds: 2592000 },
+        { label: 'week', seconds: 604800 },
+        { label: 'day', seconds: 86400 },
+        { label: 'hour', seconds: 3600 },
+        { label: 'minute', seconds: 60 },
+        { label: 'second', seconds: 1 },
+      ];
+      for (const interval of intervals) {
+        const count = Math.floor(seconds / interval.seconds);
+        if (count > 0) {
+          return `${count} ${interval.label}${count > 1 ? 's' : ''} ago`;
+        }
+      }
+      return 'just now';
+    };
+
+    let urlData = [];
+    let sortOrder = {
+      originalUrl: 'asc',
+      dateCreated: 'asc'
+    };
+
+    const fetchUrls = async () => {
+      try {
+        const response = await fetch(`http://34.76.194.134:5284/Urls/${token}`);
+        const data = await response.json();
+        urlData = data.urls; // Store the fetched data in a variable
+        displayUrls(urlData); // Display the data
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    };
+
+    const fetchFavicon = (url) => {
+      const domain = new URL(url).hostname;
+      return `https://www.google.com/s2/favicons?domain=${domain}`;
+    };
+
+    const displayUrls = (urls) => {
+      const tableBody = document.getElementById('url-table-body');
+      tableBody.innerHTML = ''; // Clear existing rows
+
+      urls.forEach((url) => {
+        const faviconUrl = fetchFavicon(url.originalUrl); // Get favicon URL
+        const row = document.createElement('tr');
+        row.innerHTML = `
+      <td><img src="${faviconUrl}" alt="favicon" style="width: 16px; height: 16px; margin-right: 8px;">${url.originalUrl}</td>
+      <td><a href="http://localhost:3000/${url.shortUrl}" target="_blank">http://localhost:3000/${url.shortUrl}</a></td>
+      <td>${timeAgo(url.dateCreated)}</td>
+      <td>${url.description}</td> <!-- Add description here -->
+      <td class="dashbaord-actoins">
+        <button onclick="openModal('${url.id}')">QR Code</button>
+        <button onclick="openEditModal(${url.id}, '${url.description}')">Edit Description</button>
+        <button onclick="openDeleteModal(${url.id})">Delete</button>
+      </td>
+    `;
+        tableBody.appendChild(row);
+      });
+    };
+
+
+    const openModal = (id) => {
+      const modal = document.getElementById('qr-modal');
+      const overlay = document.getElementById('modal-overlay');
+      const qrCodeDiv = document.getElementById('qr-code');
+
+      modal.classList.add('active');
+      overlay.classList.add('active');
+
+      qrCodeDiv.innerHTML = '';
+      new QRCode(qrCodeDiv, {
+        text: `http://localhost:5284/${id}`,
+        width: 150,
+        height: 150,
+      });
+
+      modal.dataset.currentId = id;
+    };
+
+    const downloadQRCode = () => {
+      const qrCodeDiv = document.querySelector('#qr-code canvas');
+      if (qrCodeDiv) {
+        const link = document.createElement('a');
+        link.href = qrCodeDiv.toDataURL("image/png");
+        link.download = 'qr-code.png';
+        link.click();
+      }
+    };
+
+    const closeModal = () => {
+      const modal = document.getElementById('qr-modal');
+      const overlay = document.getElementById('modal-overlay');
+      modal.classList.remove('active');
+      overlay.classList.remove('active');
+    };
+
+    let deleteUrlId = null;
+
+    const openDeleteModal = (id) => {
+      deleteUrlId = id;
+      document.getElementById('delete-modal').classList.add('active');
+      document.getElementById('delete-modal-overlay').classList.add('active');
+    };
+
+    const closeDeleteModal = () => {
+      document.getElementById('delete-modal').classList.remove('active');
+      document.getElementById('delete-modal-overlay').classList.remove('active');
+    };
+
+    const confirmDelete = async () => {
+      if (deleteUrlId) {
+        try {
+          const response = await fetch(`http://34.76.194.134:5284/api/Url/${deleteUrlId}`, {
+            method: 'DELETE',
+            headers: {
+              'Authorization': `Bearer ${token}`,
+            },
+          });
+          if (response.ok) {
+            showToast('URL deleted successfully!');
+            fetchUrls(); // Refresh the list
+          } else {
+            showToast('Error deleting URL', 'error');
+          }
+        } catch (error) {
+          showToast('Error deleting URL', 'error');
+        }
+        closeDeleteModal(); // Close modal after action
+      }
+    };
+
+    const openEditModal = (id, description) => {
+      document.getElementById('edit-description-input').value = description;
+      document.getElementById('edit-modal').classList.add('active');
+      document.getElementById('edit-modal-overlay').classList.add('active');
+      window.editUrlId = id;
+    };
+
+    const closeEditModal = () => {
+      document.getElementById('edit-modal').classList.remove('active');
+      document.getElementById('edit-modal-overlay').classList.remove('active');
+    };
+
+    const updateDescription = async () => {
+      const newDescription = document.getElementById('edit-description-input').value;
+      if (newDescription) {
+        try {
+          const response = await fetch(`http://34.76.194.134:5284/api/Url/${window.editUrlId}?description=${encodeURIComponent(newDescription)}`, {
+            method: 'PUT',
+            headers: {
+              'accept': '*/*',
+            },
+          });
+          if (response.ok) {
+            showToast('Description updated successfully!');
+            fetchUrls(); // Refresh the list
+          } else {
+            showToast('Error updating description', 'error');
+          }
+        } catch (error) {
+          showToast('Error updating description', 'error');
+        }
+        closeEditModal(); // Close the modal after saving
+      }
+    };
+
+    const showToast = (message, type = 'success') => {
+      const toast = document.createElement('div');
+      toast.classList.add('toast');
+      toast.classList.add(type);
+      toast.textContent = message;
+
+      const toastContainer = document.getElementById('toast-container');
+      toastContainer.appendChild(toast);
+
+      // Remove the toast after 3 seconds
+      setTimeout(() => {
+        toast.remove();
+      }, 3000); // Remove the toast after 3 seconds
+    };
+
+
+
+    const sortTable = (column) => {
+      let sortedData = [...urlData];
+      let sortDir = sortOrder[column] === 'asc' ? 'desc' : 'asc';
+      sortOrder[column] = sortDir;
+
+      // Update the header arrow direction
+      const headers = document.querySelectorAll('th');
+      headers.forEach((header) => {
+        header.classList.remove('sorted-asc', 'sorted-desc');
+      });
+
+      const currentHeader = document.getElementById(`${column}-header`);
+      if (sortDir === 'asc') {
+        currentHeader.classList.add('sorted-asc');
+      } else {
+        currentHeader.classList.add('sorted-desc');
+      }
+
+      if (column === 'originalUrl') {
+        sortedData.sort((a, b) => sortDir === 'asc' ? a.originalUrl.localeCompare(b.originalUrl) : b.originalUrl.localeCompare(a.originalUrl));
+      } else if (column === 'dateCreated') {
+        sortedData.sort((a, b) => sortDir === 'asc' ? new Date(a.dateCreated) - new Date(b.dateCreated) : new Date(b.dateCreated) - new Date(a.dateCreated));
+      }
+
+      displayUrls(sortedData); // Re-render the table with sorted data
+    };
+
+    window.onload = () => {
+      fetchUrls();
+    };
+  </script>
+</body>
+
+</html>
---
+++ pages/urlshorterner.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input URLs</title>
+    <link rel="stylesheet" href="search.css">
+    <link rel="stylesheet" href="dashboard.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nokora:wght@100;300;400;700;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+            background-color: #f4f4f9;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .containerUrl {
+            background-color: white;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            width: 100%;
+            max-width: 500px;
+        }
+
+        .inputUrl {
+            width: 100%;
+            padding: 12px;
+            margin: 10px 0;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            font-size: 16px;
+            box-sizing: border-box;
+        }
+
+        .button {
+            width: 100%;
+            padding: 12px;
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            font-size: 16px;
+            cursor: pointer;
+            border-radius: 8px;
+            transition: background-color 0.3s;
+        }
+
+        .button:hover {
+            background-color: #45a049;
+        }
+
+        .message {
+            margin-top: 10px;
+            font-size: 14px;
+        }
+
+        .shortened-url-container {
+            margin-top: 20px;
+        }
+
+        .qr-code-container {
+            margin-top: 20px;
+            text-align: center;
+        }
+
+        #qr-code {
+            margin: 10px auto;
+            max-width: 200px;
+        }
+
+        .download-btn {
+            display: inline-block;
+            margin-top: 10px;
+            padding: 8px 16px;
+            background-color: #007bff;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .download-btn:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <div class="containerUrl">
+        <input type="text" id="url-input" placeholder="Enter URL" class="inputUrl" />
+        <input type="text" id="description-input" placeholder="Enter Description" class="inputUrl" />
+        <button class="button" onclick="shortenUrl()">Shorten URL</button>
+        <div id="message" class="message"></div>
+        <div id="shortened-url-container" class="shortened-url-container"></div>
+        <div id="qr-code-container" class="qr-code-container"></div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+<script>
+  const token = localStorage.getItem('authToken');
+  
+  // Regex for URL validation
+  const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+
+  const shortenUrl = async () => {
+    const url = document.getElementById('url-input').value;
+    const description = document.getElementById('description-input').value;
+    const messageElement = document.getElementById('message');
+    const shortenedUrlContainer = document.getElementById('shortened-url-container');
+    const qrCodeContainer = document.getElementById('qr-code-container');
+    
+    // Validate URL format
+    if (!urlRegex.test(url)) {
+      messageElement.textContent = "Please enter a valid URL.";
+      messageElement.style.color = "red";
+      return;
+    }
+
+    if (!token) {
+      messageElement.textContent = "No authentication token found. Please log in.";
+      messageElement.style.color = "red";
+      return;
+    }
+
+    try {
+      const encodedUrl = encodeURIComponent(url);
+      const encodedDescription = encodeURIComponent(description);
+      
+      const response = await fetch(
+        `http://34.76.194.134:5284/api/URL?url=${encodedUrl}&token=${token}&description=${encodedDescription}`,
+        { method: 'POST' }
+      );
+
+      const data = await response.text(); // Read response as plain text
+
+      if (response.ok) {
+        messageElement.textContent = "URL successfully shortened!";
+        messageElement.style.color = "green";
+        
+        const shortenedUrl = data.trim(); // Assuming the response is just the ID
+        const shortenedLink = `http://localhost:3000/${shortenedUrl}`;
+        
+        const linkElement = document.createElement('a');
+        linkElement.href = shortenedLink;
+        linkElement.textContent = `Shortened URL: ${shortenedLink}`;
+        linkElement.target = "_blank"; // Open in a new tab
+        shortenedUrlContainer.innerHTML = ""; // Clear previous link
+        shortenedUrlContainer.appendChild(linkElement);
+
+        // Generate QR Code only if QRCode.js is loaded
+        if (shortenedLink) {
+          generateQrCode(shortenedLink);
+        } else {
+          messageElement.textContent = "QR Code generation failed. Please try again.";
+          messageElement.style.color = "red";
+        }
+
+        document.getElementById('description-input').value = ""; // Clear description input
+        setTimeout(() => {
+          window.location.href = "links.html"; // Redirect to links page
+        }, 1500); // Redirect after 1.5 seconds
+      } else {
+        console.error("Error response:", data);
+        messageElement.textContent = `Error: ${data || 'Failed to shorten URL'}`;
+        messageElement.style.color = "red";
+      }
+    } catch (error) {
+      console.error("Fetch Error:", error);
+      messageElement.textContent = "Error shortening URL: " + error.message;
+      messageElement.style.color = "red";
+    }
+  };
+
+const generateQrCode = (url) => {
+    const qrCodeContainer = document.getElementById('qr-code-container');
+    
+    // Ensure QRCode.js is available
+    if (typeof QRCode !== "undefined") {
+      // Clear previous QR code if any
+      qrCodeContainer.innerHTML = "";
+
+      // Create the QR code
+      const qrCode = new QRCode(qrCodeContainer, {
+        text: url,
+        width: 128,
+        height: 128,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+      });
+
+      // Wait for the QR code image to load, then add the download link
+      qrCodeContainer.querySelector("img").onload = () => {
+        const qrImage = qrCodeContainer.querySelector("img");
+        const downloadButton = document.createElement('a');
+        downloadButton.href = qrImage.src;
+        downloadButton.download = "qr_code.png";
+        downloadButton.textContent = "Download QR Code";
+        downloadButton.classList.add("download-btn");
+
+        qrCodeContainer.appendChild(downloadButton);
+      };
+    } else {
+      console.error("QRCode.js is not available.");
+    }
+  };
+
+</script>
+
+</body>
+</html>
